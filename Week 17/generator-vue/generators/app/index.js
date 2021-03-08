41144177282a9779cfa5b6cc937ab74849d70ecc
zzy <zhouzhenyu@shuinfo.com>
2021-03-08 13:49:32 +0800

    week17周作业

---
+++ Week 17/generator-vue/generators/app/index.js
@@ -0,0 +1,64 @@
+var Generator = require('yeoman-generator');
+
+module.exports = class extends Generator {
+    // The name `constructor` is important here
+  constructor(args, opts) {
+    // Calling the super constructor is important so our generator is correctly set up
+    super(args, opts);
+
+    // Next, add your custom code
+    // this.option('babel'); // This method adds support for a `--babel` flag
+  }
+
+  async initPackage() {
+    let answer = await this.prompt([
+      {
+        type: "input",
+        name: "name",
+        message: "Your project name",
+        default: this.appname // Default to current folder name
+      }
+    ])
+    const pkgJson = {
+      "name": answer.name,
+      "version": "1.0.0",
+      "description": "",
+      "main": "generators/app/index.js",
+      "scripts": {
+        "test": "echo \"Error: no test specified\" && exit 1"
+      },
+      "author": "",
+      "license": "ISC",
+        "devDependencies": {
+          
+        },
+        "dependencies": {
+        }
+      };
+
+      // Extend or create package.json file in destination path
+        this.fs.extendJSON(this.destinationPath('package.json'), pkgJson);
+        this.npmInstall(['vue'], { 'save-dev': false });
+        this.npmInstall(['webpack', 'vue-loader', 'vue-style-loader',
+        'css-loader', 'vue-template-compiler', 'copy-webpack-plugin'], { 'save-dev': true });
+  
+    this.fs.copyTpl(
+      this.templatePath('HelloWorld.vue'),
+      this.destinationPath('src/HelloWorld.vue')
+    );
+    this.fs.copyTpl(
+      this.templatePath('webpack.config.js'),
+      this.destinationPath('webpack.config.js')
+    );
+    this.fs.copyTpl(
+      this.templatePath('main.js'),
+      this.destinationPath('src/main.js')
+    );
+    this.fs.copyTpl(
+      this.templatePath('index.html'),
+      this.destinationPath('src/index.html'),
+      {title: answer.name}
+    );
+  }
+
+};
---
+++ Week 17/toolchain/generators/app/index.js
@@ -0,0 +1,36 @@
+var Generator = require('yeoman-generator');
+
+module.exports = class extends Generator {
+    // The name `constructor` is important here
+  constructor(args, opts) {
+    // Calling the super constructor is important so our generator is correctly set up
+    super(args, opts);
+
+    // Next, add your custom code
+    // this.option('babel'); // This method adds support for a `--babel` flag
+  }
+
+  initPackage() {
+    const pkgJson = {
+        devDependencies: {
+          eslint: '^3.15.0'
+        },
+        dependencies: {
+          react: '^16.2.0'
+        }
+      };
+
+      // Extend or create package.json file in destination path
+        this.fs.extendJSON(this.destinationPath('package.json'), pkgJson);
+        this.npmInstall();
+  }
+
+  writing() {
+    this.fs.copyTpl(
+      this.templatePath('index.html'),
+      this.destinationPath('public/index.html'),
+      { title: 'Templating with Yeoman' }
+    );
+  }
+
+};
